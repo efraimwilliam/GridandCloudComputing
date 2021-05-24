b73f3a6ea8bc26b9c35c54b2c8c6c23db27a8f7c
efraimwilliam <efraimwilliam@gmail.com>
2021-05-24 15:28:22 +0800

    test deploy

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('profile');
+            $table->string('profile')->nullable();
         });
     }
 
